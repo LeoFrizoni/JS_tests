f896ef8c9791c9bd61f3f225154b338786cd8d82
Leonardo Frizoni Lawall <128152717+LeoFrizoni@users.noreply.github.com>
2025-01-31 11:35:36 -0300

    Add files via upload

---
+++ coffee.js
@@ -0,0 +1,41 @@
+var grmcoffee = window.querySelector("input#grmcoffee");
+var mlwater = window.querySelector("input#lmwater");
+var output = window.querySelector("#output")
+
+function boilwater(currentTemp){
+    while(currentTemp < 92 || currentTemp >96 ){
+        if (currentTemp < 92){
+            output.textContent += "Boiling water..."
+            currentTemp += 1 //Simulate boiling
+        }
+        else if (currentTemp > 96 ){
+            output.textContent = "Cooling water..."
+            currentTemp -= 1 //Simulate cooling
+        }
+        sleep(100); //Simulate pause for boiling or cooling
+    }
+    output.textContent += `The actual Water temperature is ${currentTemp}°C`
+    return currentTemp;
+}
+
+async function docoffee(){ //async function for waiting boilwater()
+    var coffee = parseFloat(grmcoffee.value);
+    var water = parseFloat(mlwater.value);
+    if (isNaN(coffee) || isNaN(water) || coffee <= 0 ||  water <= 0)
+        output.textContent = "Please, insert valid numbers for Coffee amount and Water amount";
+        
+    else{
+        output.textContent = "Measuring Coffee amount..."
+        output.textContent = "Measuring Water amount..."
+        var initialTemp = 32
+        var finalTemp = await boilwater(initialTemp)
+        output.textContent = "Boiling water..."
+        output.textContent = "Pouring hot water into the filter with coffee..."
+        output.textContent = "Starting to prepair your coffee..."
+    
+    }    
+}
+
+   
+
+
